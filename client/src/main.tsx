6e65ba85e2ae1aed538829b5e63781f6f576d911
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2023-07-03 19:03:02 +0700

    unuse import react?

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
