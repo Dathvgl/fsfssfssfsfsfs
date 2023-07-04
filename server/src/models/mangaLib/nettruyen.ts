96184477ac486f014cadd5277d6efd6c218e03d8
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2023-07-04 12:58:07 +0700

    networkidle0

--- server/src/models/mangaLib/nettruyen.ts
+++ server/src/models/mangaLib/nettruyen.ts
@@ -78,7 +78,7 @@ export class Nettruyen implements AbstractMangaFactory {
       `${this.baseUrl}/tim-truyen?keyword=${keyword}${
         page > 1 ? `&page=${page}` : ``
       }`,
-      { waitUntil: "networkidle2" }
+      { waitUntil: "networkidle0" }
     );
 
     const element = await _page.$$(
@@ -189,7 +189,7 @@ export class Nettruyen implements AbstractMangaFactory {
     } else if (page !== undefined) {
       path += `?page=${page}`;
     }
-    await _page.goto(`${this.baseUrl}${path}`, { waitUntil: "networkidle2" });
+    await _page.goto(`${this.baseUrl}${path}`, { waitUntil: "networkidle0" });
     const element = await _page.$$(
       "#ctl00_divCenter > div.Module.Module-170 > div > div.items > div > div.item > figure"
     );
@@ -282,7 +282,7 @@ export class Nettruyen implements AbstractMangaFactory {
     path = path !== undefined ? path : "";
 
     const _page = await (await this.browser).newPage();
-    await _page.goto(url_chapter, { waitUntil: "networkidle2" });
+    await _page.goto(url_chapter, { waitUntil: "networkidle0" });
     const content = await _page.$(
       "#ctl00_divCenter > div > div.reading-detail.box_doc"
     );
@@ -370,7 +370,7 @@ export class Nettruyen implements AbstractMangaFactory {
 
   async getDetailManga(url: string): Promise<ResponseDetailManga> {
     const _page = await (await this.browser).newPage();
-    await _page.goto(url, { waitUntil: "networkidle2" });
+    await _page.goto(url, { waitUntil: "networkidle0" });
     const content = await _page.$("#ctl00_divCenter");
     const title = await content!.$eval("article > h1", (el) => el.textContent);
     const path = url.substring(`${this.baseUrl}`.length);
@@ -498,7 +498,7 @@ export class Nettruyen implements AbstractMangaFactory {
   async getListLatestUpdate(page = 1): Promise<ResponseListManga> {
     const _page = await (await this.browser).newPage();
     await _page.goto(`${this.baseUrl}${page > 1 ? `/?page=${page}` : ``}`, {
-      waitUntil: "networkidle2",
+      waitUntil: "networkidle0",
     });
 
     const element = await _page.$$(
